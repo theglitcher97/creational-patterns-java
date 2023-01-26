8ec5a576c92bc44c7b380c3fe50ec82f4014c022
Fernando Orozco <forozcomercado@gmail.com>
2023-01-26 09:59:57 -0500

    ref: remove unused code

--- src/main/java/patterns/design/creational_patterns/factory/App.java
+++ src/main/java/patterns/design/creational_patterns/factory/App.java
@@ -1,7 +1,6 @@
 package patterns.design.creational_patterns.factory;
 
 import patterns.design.creational_patterns.factory.concrete_swords.Sword;
-import patterns.design.creational_patterns.factory.enums.EnchantType;
 import patterns.design.creational_patterns.factory.enums.SwordType;
 
 public class App {
@@ -16,14 +15,6 @@ public class App {
 	public static void testSword(Sword sword){
 		System.out.println();
 		System.out.println("Current damage " + sword.getDamage());
-		System.out.println("Adding enchant of type: "+EnchantType.EPIC.name());
-		sword.addEnchanted(EnchantType.EPIC);
-		System.out.println("Adding enchant of type: "+EnchantType.WEAK.name());
-		sword.addEnchanted(EnchantType.WEAK);
-		System.out.println("Current damage " + sword.getDamage());
-		sword.attack();
-		System.out.println("Removing enchant of type: "+EnchantType.WEAK.name());
-		sword.removeEnchanted(EnchantType.WEAK);
 		sword.attack();
 	}
 }
